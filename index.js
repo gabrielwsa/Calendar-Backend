9eb18ca367a3f7aa15b9c29d2380d93f059444c4
gabriel silva <gabrielsilvadearaujo45@gmail.com>
2025-04-25 15:32:49 -0300

    atenticacion finalizada

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { dbConnection } = require("./database/config");
+const cors = require('cors');
+
 dotenv.config();
 
 //* CREAR SERVIDOR EXPRESS
@@ -9,14 +11,19 @@ const app = express();
 //* Conectar a la base de datos
 dbConnection();
 
+//* CORS - configuracao basica seria somente agregar ele a todas as rotas
+app.use(cors());
+
 //* ROUTE PUBLIC
 app.use(express.static("public"));
+
 //* parsear body
 app.use(express.json());
 
 //* ROUTER
 //* auth: criar, login, renew (Endpoint: /auth)
 app.use('/api/auth', require('./routes/auth'));
+
 //* crud: eventos
 
 
