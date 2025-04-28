5585ccc147fe46b9bf2eb6933414725325f06106
gabriel silva <gabrielsilvadearaujo45@gmail.com>
2025-04-28 08:32:57 -0300

    fix

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ dbConnection();
 //* CORS - configuracao basica seria somente agregar ele a todas as rotas
 app.use(cors({
     origin: 'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 //* ROUTE PUBLIC
