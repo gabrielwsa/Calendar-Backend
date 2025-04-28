1c7a92ce35ccece5deda8ce77a7575f0e54dabdd
gabriel silva <gabrielsilvadearaujo45@gmail.com>
2025-04-28 08:31:22 -0300

    credenciales localhost

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const app = express();
 dbConnection();
 
 //* CORS - configuracao basica seria somente agregar ele a todas as rotas
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 //* ROUTE PUBLIC
 app.use(express.static("public"));
